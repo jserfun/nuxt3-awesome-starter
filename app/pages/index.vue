41b9805d6e1096e9bfdfdc533aa7d55d874396a2
jserfun <190422495+jserfun@users.noreply.github.com>
2024-12-22 05:58:11 +0800

    feat: test mv pages into app

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+const { awesome } = useAppConfig()
+definePageMeta({ layout: 'default' })
+useHead({ titleTemplate: '', title: awesome?.name || 'Nuxt 3 Awesome Starter' })
+</script>
+
+<template>
+  <div>haha</div>
+</template>
